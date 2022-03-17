7b1e2a9c229e5e16dbc9d75ef9c181080b4e5be1
Kamiel BreesÃ <kamiel.brees@gmail.com>
2022-03-17 11:50:17 +0100

    fix

--- autossh_stunnel.sh
+++ autossh_stunnel.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 while true; do
-    /usr/bin/autossh -p 443 -M 20000 -N -R 10022:localhost:22 root@localhost
+    /usr/bin/autossh -p 443 -M 0 -N -R 10022:localhost:22 root@localhost
     sleep 5
 done
