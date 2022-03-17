32fd871cd36756b27af4fcfab5fa54d210c76f1a
Kamiel BreesÃ <kamiel.brees@gmail.com>
2022-03-17 14:18:13 +0100

    final fix

--- autossh_ptunnel.sh
+++ autossh_ptunnel.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 while true; do
-    /usr/bin/autossh -p 8888 -M 0 -N -R 12222:localhost:22 root@localhost
+    /usr/bin/autossh -p 8888 -M 0 -N -R 12222:localhost:22 -o "StrictHostKeyChecking no" root@localhost
     sleep 5
 done
