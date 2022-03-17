04c58184da8870e8734adc04bf535a906627dd68
Kamiel BreesÃ <kamiel.brees@gmail.com>
2022-03-17 11:21:46 +0100

    update htc command

--- httptunnel.sh
+++ httptunnel.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 while true; do
-    runuser -l hackerman -c 'nohup /usr/bin/htc --no-daemon -F 1234 $SERVERIP:8080 -k 10'
+    sudo -u hackerman bash -c '/usr/bin/htc --no-daemon -F 1234 $SERVERIP:8080 -k 10'
     sleep 5
 done
