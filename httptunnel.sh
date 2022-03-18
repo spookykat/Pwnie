2f7ec414a7231bc2ef475487ae42c9f09b8ccb70
Kamiel Brees <kamiel.brees@gmail.com>
2022-03-18 11:24:51 +0100

    username

--- httptunnel.sh
+++ httptunnel.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 while true; do
-    sudo -u hackerman bash -c 'nohup /usr/bin/htc --no-daemon -F 1234 $SERVERIP:8080 -k 10'
+    sudo -u pwnie bash -c 'nohup /usr/bin/htc --no-daemon -F 1234 $SERVERIP:8080 -k 10'
     sleep 5
 done
