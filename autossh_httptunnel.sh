98d34c34a8d804c8277d9eedc9434d02942036a4
Kamiel BreesÃ <kamiel.brees@gmail.com>
2022-03-24 09:32:05 +0100

    fix

--- autossh_httptunnel.sh
+++ autossh_httptunnel.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 while true; do
-    /usr/bin/autossh -p 1234 -M 0 -N -R 10$n23:localhost:22 -o "StrictHostKeyChecking no" root@localhost
+    /usr/bin/autossh -p 1234 -M 0 -N -R 10${n}23:localhost:22 -o "StrictHostKeyChecking no" root@localhost
     sleep 5
 done
--- autossh_ptunnel.sh
+++ autossh_ptunnel.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 while true; do
-    /usr/bin/autossh -p 8888 -M 0 -N -R 10$n24:localhost:22 -o "StrictHostKeyChecking no" root@localhost
+    /usr/bin/autossh -p 8888 -M 0 -N -R 10${n}24:localhost:22 -o "StrictHostKeyChecking no" root@localhost
     sleep 5
 done
